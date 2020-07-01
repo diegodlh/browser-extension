diegodlh/browser-extension

commit e9acd7b8df7f2cb5052e309d2b0ddd59c1ad7864

Add -self suffix to self-distributed versions
Mozilla Addons does let to distribute both AMO and self-distributed versions
of an addon, but they cannot have the same version numbers.
See https://blog.mozilla.org/addons/2017/01/26/mixing-listed-and-unlisted-on-amo/

tools/settings.js
@@ -26,14 +26,19 @@ process.stdout.on('error', err => {
  *   otherwise set it to a string of the form "gXXXXXXX[.dirty]" to reflect the
  *   exact commit and state of the repository.
  */
-function getVersion(buildType) {
+function getVersion(buildType, selfDistribution) {
   const gitInfo = gitDescribeSync();
 
   if (buildType === 'production' && gitInfo.dirty) {
     throw new Error('cannot create production build with dirty git state!');
   }
 
-  const version = `${gitInfo.tag.substr(1)}`;
+  let version = `${gitInfo.tag.substr(1)}`;
+
+  if (selfDistribution) {
+    version = version + '-self';
+  }
+
   let versionName = 'Unofficial Build';
 
   if (buildType !== 'production') {
@@ -49,6 +54,6 @@ if (process.argv.length !== 3) {
 }
 
 const settings = require(path.join(process.cwd(), process.argv[2]));
-const settingsOut = Object.assign({}, settings, getVersion(settings.buildType));
+const settingsOut = Object.assign({}, settings, getVersion(settings.buildType, settings.selfDistribution));
 
 console.log(JSON.stringify(settingsOut));

commit fb7e04f98f105e017f5d80aa6fb6ae7b11521900

Fix bug introduced by e9acd7b: remove `-` from -self suffix
Mozilla Addon version parts must follow the <number-a><string-b><number-c><string-d> format,
where strings must be non-numeric ASCII characters.
See https://developer.mozilla.org/en-US/docs/Mozilla/Toolkit_version_format

tools/settings.js
@@ -36,7 +36,7 @@ function getVersion(buildType, selfDistribution) {
   let version = `${gitInfo.tag.substr(1)}`;
 
   if (selfDistribution) {
-    version = version + '-self';
+    version = version + 'self';
   }
 
   let versionName = 'Unofficial Build';