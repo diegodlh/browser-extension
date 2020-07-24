1a9415b7178feb3eb697725b3804e06706f73336
Diego de la Hera <delahera@gmail.com>
2020-07-23 23:48:09 -0300

    Implement openSidebar extra feature

--- src/options/options.js
+++ src/options/options.js
@@ -3,6 +3,7 @@
 function saveOptions() {
   chrome.storage.sync.set({
     badge: document.getElementById('badge').checked,
+    openSidebar: document.getElementById('openSidebar').checked,
   });
 }
 
@@ -10,12 +11,20 @@ function loadOptions() {
   chrome.storage.sync.get(
     {
       badge: true,
+      openSidebar: false,
     },
     function (items) {
       document.getElementById('badge').checked = items.badge;
+      document.getElementById('openSidebar').checked = items.openSidebar;
     }
   );
 }
 
 document.addEventListener('DOMContentLoaded', loadOptions);
 document.getElementById('badge').addEventListener('click', saveOptions);
+
+document.getElementById('openSidebar').addEventListener('click', (e) => {
+  chrome.storage.sync.set({
+    openSidebar: e.target.checked,
+  })
+});
