dbb02269ab755ad4992dd8c2ddcd793c508eba05
Diego de la Hera <delahera@gmail.com>
2020-07-23 23:55:32 -0300

    Implement experimental new page-note button extra feature

--- src/options/options.js
+++ src/options/options.js
@@ -10,12 +10,21 @@ function loadOptions() {
   chrome.storage.sync.get(
     {
       badge: true,
+      enableExperimentalNewNoteButton: false,
     },
     function (items) {
       document.getElementById('badge').checked = items.badge;
+      document.getElementById('enableExperimentalNewNoteButton'
+        ).checked = items.enableExperimentalNewNoteButton;
     }
   );
 }
 
 document.addEventListener('DOMContentLoaded', loadOptions);
 document.getElementById('badge').addEventListener('click', saveOptions);
+
+document.getElementById('enableExperimentalNewNoteButton').addEventListener('click', (e) => {
+  chrome.storage.sync.set({
+    enableExperimentalNewNoteButton: e.target.checked,
+  })
+});
