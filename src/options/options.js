88b39693fc4e8f66945c444964032fc2d2530fcf
Diego de la Hera <delahera@gmail.com>
2020-07-23 23:39:58 -0300

    Support display current group name below adder toolbar

--- src/options/options.js
+++ src/options/options.js
@@ -10,12 +10,20 @@ function loadOptions() {
   chrome.storage.sync.get(
     {
       badge: true,
+      adderToolbarFooter: false,
     },
     function (items) {
       document.getElementById('badge').checked = items.badge;
+      document.getElementById('adderToolbarFooter').checked = items.adderToolbarFooter;
     }
   );
 }
 
 document.addEventListener('DOMContentLoaded', loadOptions);
 document.getElementById('badge').addEventListener('click', saveOptions);
+
+document.getElementById('adderToolbarFooter').addEventListener('click', (e) => {
+  chrome.storage.sync.set({
+    adderToolbarFooter: e.target.checked,
+  })
+});
