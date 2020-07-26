5278abd2641f1e30b2ff21d651dec5be4fdb6b9f
Diego de la Hera <delahera@gmail.com>
2020-07-25 21:28:08 -0300

    Support enabling white text overlay in PDFs

--- src/options/options.js
+++ src/options/options.js
@@ -10,12 +10,20 @@ function loadOptions() {
   chrome.storage.sync.get(
     {
       badge: true,
+      usePdfWhiteOverlay: false,
     },
     function (items) {
       document.getElementById('badge').checked = items.badge;
+      document.getElementById('usePdfWhiteOverlay').checked = items.usePdfWhiteOverlay;
     }
   );
 }
 
 document.addEventListener('DOMContentLoaded', loadOptions);
 document.getElementById('badge').addEventListener('click', saveOptions);
+
+document.getElementById('usePdfWhiteOverlay').addEventListener('click', (e) => {
+  chrome.storage.sync.set({
+    usePdfWhiteOverlay: e.target.checked,
+  })
+});
